acdffcada916ffcd8dc68030aec46bd91c8cf4dd
H. Joe Lee <hyoklee@hdfgroup.org>
2025-07-01 14:32:36 -0500

    ci(conda): add

---
+++ build.sh
@@ -0,0 +1,11 @@
+export CMAKE_PREFIX_PATH=$PREFIX
+export CMAKE_INCLUDE_PATH=$PREFIX/include
+# export PKG_CONFIG_PATH=$CONDA_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
+mkdir build
+cd build
+cmake \
+    -DMERCURY_USE_BOOST_PP=ON \
+    -DNA_USE_OFI=OFF \
+    -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX ..
+cmake --build . --config Release 
+cmake --install . --config Release 

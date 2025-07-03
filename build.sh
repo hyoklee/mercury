971e56c6effaa59328fb40c474ffd8011b3ad3af
H. Joe Lee <hyoklee@hdfgroup.org>
2025-07-02 22:50:58 -0500

    build: shared on

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 export CMAKE_PREFIX_PATH=$PREFIX
 export CMAKE_INCLUDE_PATH=$PREFIX/include
-# export PKG_CONFIG_PATH=$CONDA_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
 mkdir build
 cd build
 cmake \
+    -DBUILD_SHARED_LIBS=ON \
     -DMERCURY_USE_BOOST_PP=ON \
     -DNA_USE_OFI=OFF \
+    -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
     -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX ..
 cmake --build . --config Release 
 cmake --install . --config Release 
